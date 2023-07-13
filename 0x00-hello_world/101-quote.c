321b9bd72da7fc6663b2bfefea0cabdd1c14c82b
Israelshecktar <iamshecktar1996@gmail.com>
2023-07-13 06:26:44 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdout>
+/**
+ * main - this program prints in c
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
