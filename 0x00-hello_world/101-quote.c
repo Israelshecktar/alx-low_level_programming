69067757ca400024d7d7ecede5fe40ad74bdbc75
Israelshecktar <iamshecktar1996@gmail.com>
2023-07-13 06:52:56 -0700

    lovely

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 1 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
