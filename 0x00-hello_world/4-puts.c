e940e73d5cadfbee773e389ce12f74899aab4828
Israelshecktar <iamshecktar1996@gmail.com>
2023-07-13 01:28:34 -0700

    i love C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 /*
  *  main - This program runs in betty format, return: 0 (means success)!
 */
-int main()
-{
+int main(void) {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
