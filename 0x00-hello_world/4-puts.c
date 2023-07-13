0d6fb387c607ca9bac0751ea3ed0ff9b06b9c5a9
Israelshecktar <iamshecktar1996@gmail.com>
2023-07-13 00:56:32 -0700

    i love C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*This program runs in bettybn format and it entails the file 4-puts.c, return (0)= success!*/
-int main(void);
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle, followed by a new line");
 return (0);
