2cd9aa0a5f5e4ebf0e036891cd29de3e68bc7be9
Israelshecktar <iamshecktar1996@gmail.com>
2023-07-16 11:51:43 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char e;
+
+	e = 'a';
+	while
+		(e <= 'z') {
+			putchar(e);
+			e++;
+		}
+	putchar('\n');
+	return (0);
+}
