a5322f4b49856744b2b9d4c29c72b2b3023c968c
Israelshecktar <iamshecktar1996@gmail.com>
2023-07-16 12:08:43 -0700

    betty

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	char e;
+
+	char i;
+
+	e = 'a';
+	i = 'A';
+	while
+		(e <= 'z') {
+			putchar(e);
+			e++;
+		}
+	while
+		(i <= 'z') {
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
