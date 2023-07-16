1b7323cb5465159db97ebcffbf6d65e9107ea242
Israelshecktar <iamshecktar1996@gmail.com>
2023-07-16 13:46:30 -0700

    betty

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 success
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+return (0);
+}
