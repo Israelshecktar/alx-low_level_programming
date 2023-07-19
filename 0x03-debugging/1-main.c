cc633d259dc1c17af21a7d97ee4f6b774f2e22af
Israelshecktar <iamshecktar1996@gmail.com>
2023-07-19 06:44:05 -0700

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
 	/*putchar(i);*/
@@ -18,4 +20,5 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
+	
 }
