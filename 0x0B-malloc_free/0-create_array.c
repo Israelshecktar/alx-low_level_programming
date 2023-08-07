664b360ec890ce6473cb75fe9b040972f3c3603c
Israelshecktar <iamshecktar1996@gmail.com>
2023-08-07 07:19:17 -0700

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,20 +11,21 @@
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
+	char *array;
+	unsigned int i;
 
+	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *array = (char *) malloc(sizeof(char) * size);
+	array = (char *) malloc(sizeof(char) * size);
 
 	if (array == NULL)
 
 	{
 		return (NULL);
 	}
-
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
