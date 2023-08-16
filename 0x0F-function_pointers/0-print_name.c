8f82971c0c512de95e44afcb8dab7acf8f2e38fc
Israelshecktar <iamshecktar1996@gmail.com>
2023-08-16 10:04:45 -0700

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name - Function print name
+ * @name: first name to be printed
+ * @f: holds function to the second name
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+/**
+ * print - second function that prints name
+ * @name: prints name
+ * Return: Nothing
+ */
+void print(char *name)
+{
+	printf("Name: %s\n", name);
+}
