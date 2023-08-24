e29d8cc5de2ddc0f44c8416d5a5f197cd7e7f53f
Israelshecktar <iamshecktar1996@gmail.com>
2023-08-24 14:40:15 +0000

    Singly_linked

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - function prints list of elements
+ * @h: strings to be counted
+ * @list_t: constant nodes of strings
+ * Return: NULL or str
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		count++;
+		h = h->nxt_node;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This file discusses singly_linked_list in programminng and examples of how it can be used as a better substitute to Arrays.
