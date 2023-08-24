659f67deb3fb21662aa5aacdcac58ae7d196432c
Israelshecktar <iamshecktar1996@gmail.com>
2023-08-24 15:46:58 +0000

    Singly_link

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,9 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - function prints list of elements
- * @h: strings to be counted
- * Return: NULL or no of nodes printed
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
