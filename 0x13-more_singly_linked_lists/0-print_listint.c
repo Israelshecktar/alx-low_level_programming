34eb8a8229c2d4347c396dd057023cb9102a4cf7
Isrealsheckatar <iamshecktar1996@gmail.com>
2023-08-28 20:17:59 +0100

    Ammended function that prints element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include "lists.h"
+
 /**
-* print_listint - function prints all elements in the node
-* @h: List of nodes int the list
-* Return: 0 success
-*/
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
