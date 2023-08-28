7522c97d4f59a9bd2787be88a4f48f4e9f096fcf
Isrealsheckatar <iamshecktar1996@gmail.com>
2023-08-28 20:37:12 +0100

    Ammended function that prints element

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node structure for alx project
  *
  */
 typedef struct listint_s
