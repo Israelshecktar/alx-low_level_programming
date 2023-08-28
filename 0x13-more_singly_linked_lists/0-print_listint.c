c806a08cdb4962f5e66ac59e3c23b286564631e4
Isrealsheckatar <iamshecktar1996@gmail.com>
2023-08-28 20:11:42 +0100

    Ammended function to print elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-
 /**
 * print_listint - function prints all elements in the node
 * @h: List of nodes int the list
@@ -11,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t c = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		c++;
+		h = h->next;
 	}
 
 	return (c);
