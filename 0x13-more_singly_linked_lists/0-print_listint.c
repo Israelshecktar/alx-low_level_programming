4de6c08dae865a376fd9ec29fd91706a05b8c96f
Isrealsheckatar <iamshecktar1996@gmail.com>
2023-08-30 14:29:42 +0100

    Ammended print_list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 	size_t nodes = 0;
 
 	while (h)
-	{
+{
 		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-	}
+}
 
-	return (nodes);
+return (nodes);
 }
