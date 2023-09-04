d3d07f8b161196604b60d91d8b5d77452bcdbeae
Isrealsheckatar <iamshecktar1996@gmail.com>
2023-09-04 13:32:57 +0100

    read_files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-			return (0);
+	{
+		return (0);
+	}
 
 	buffer = malloc(sizeof(char *) * letters);
 	n = read(fd, buffer, letters);
