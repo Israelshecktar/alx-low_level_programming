ddf7d183c59db3854cce0e785c35b9d5dafb4842
Israelshecktar <iamshecktar1996@gmail.com>
2023-10-12 12:24:56 +0100

    function prints list at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node
+ *
+ * Return: the nth node of the list, or NULL if it does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	count = 0;
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			return (head);
+		}
+		count++;
+		head = head->next;
+	}
+	return (NULL);
+}
