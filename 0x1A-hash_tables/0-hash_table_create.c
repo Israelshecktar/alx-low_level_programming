844f0dad08c7f7e7e8f9dde87a909c42c375b1c4
Israelshecktar <iamshecktar1996@gmail.com>
2023-11-02 18:39:44 +0100

    implementation of hash function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,36 +1,28 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
-
+#include <stdlib.h>
 /**
- * hash_table_create - creates a hash table
+ * hash_table_create - function that creates a hash table
  * @size: size of the array
- *
- * Return: pointer to the newly created hash table
+ * Return: Returns a pointer to the newly created hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *table;
 	unsigned long int i;
 
-	if (size == 0)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (table->array == NULL)
 		return (NULL);
 
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-	{
-		return (NULL);
-	}
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 
-	ht->size = size;
-
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-
-	return (ht);
+	return (table);
 }
