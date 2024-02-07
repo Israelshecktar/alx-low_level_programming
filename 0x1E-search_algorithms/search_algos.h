a879afcf25cfe221b2afc01acd3e15c565078d8c
Israelshecktar <iamshecktar1996@gmail.com>
2024-02-07 23:27:15 +0100

    skiplist algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -20,6 +20,27 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t low, size_t high);
@@ -34,4 +55,8 @@ void print_array_t(int *array, int start, int end);
 int binary_search_helper(int *array, int value,size_t low, size_t high);
 size_t min(size_t a, size_t b);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+void init_express(skiplist_t *list, size_t size);
+skiplist_t *create_skiplist(int *array, size_t size);
+void free_skiplist(skiplist_t *list);
 #endif /* SEARCH_ALGOS_H */
